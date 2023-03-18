e7dcde38f25a266c0823f822f3681c9c5b8f6c09
Sujon <sujonhowlader67@gmail.com>
2023-03-18 23:33:30 +0600

    first commit

---
+++ minmax/index.js
@@ -0,0 +1,14 @@
+const arr=[23,43,54,67,77,90]
+var min= arr[0];
+var max =arr[0];
+
+for(i=0;i<arr.length;i++){
+    if(arr[i]<min){
+        min =arr[i];
+    }
+    if(arr[i]>max){
+        max = arr[i];
+    }
+}
+
+console.log(max);
